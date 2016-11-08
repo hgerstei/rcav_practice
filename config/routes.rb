1962d5caffe16ca7b4a0fc66f0f878d353d986f6
Hilary Gerstein <hilary.gerstein@users.noreply.github.com>
2016-11-08 10:36:15 -0500

    Routes
    Routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   get("/", { :controller => "calculations", :action => "instructions" })
+
+  # Square (e.g., http://localhost:3000/square/8)
+  get("/square/:id", { :controller => "calculations", :action => "square" })
+
+  # Square Root (e.g., http://localhost:3000/square_root/8)
+  get("/square_root/:id", { :controller => "calculations", :action => "square_root" })
+
+  # Random Number (e.g., http://localhost:3000/random/50/100)
+  get("/random/:min/:max", { :controller => "calculations", :action => "random_number" })
+
+  # Payment (e.g., http://localhost:3000/payment/410/30/250000)
+  get("/payment/:interest_rate/:number_of_years/:principal_value", { :controller => "calculations", :action => "payment" })
+
 end
